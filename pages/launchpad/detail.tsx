60d91a0e0a50fa42ccff8cf96ee4df594d4b851d
aemoe <aemoe@icloiud.com>
2023-05-31 15:52:29 +0800

    fixed

--- pages/launchpad/detail.tsx
+++ pages/launchpad/detail.tsx
@@ -196,7 +196,7 @@ const LaunchpadDetails = () => {
               <div className={styles.title}>
                 <div>
                   <div className={styles.ori}>ArkStart</div>
-                  <div>Private Sale</div>
+                  <div>Whitelist Public Sale</div>
                 </div>
                 <div className={styles.deadline}>
                   <Timer
