def6dd0845e117da272d805a94cccee6bd8d8f78
赵开文 <763018250@qq.com>
2023-05-31 23:39:15 +0800

    feat(list): add list
    添加列表

--- pages/launchpad/index.js
+++ pages/launchpad/index.js
@@ -9,40 +9,44 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import classNames from "classnames/bind";
 import Cookies from "js-cookie";
 import { utils } from "ethers";
-import Link from 'next/link'
+import Link from "next/link";
 import Image from "next/image";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
-import { Swiper, SwiperSlide } from "swiper/react";
+import tokenimg from "../../public/images/token.png";
+import project from "../../public/launchpad/project.png";
+import avatar from "../../public/launchpad/messi.webp";
+import icon1 from "../../public/launchpad/titleicon.png";
+import icon2 from "../../public/launchpad/launchicon2.png";
+import icon3 from "../../public/launchpad/launchicon3.png";
+import icon4 from "../../public/launchpad/launchicon4.png";
+import hot from "../../public/launchpad/hot.svg";
+import github from "../../public/launchpad/github.png";
+import twitter from "../../public/launchpad/twitter.png";
+import telegram from "../../public/launchpad/telegram.png";
+import discord from "../../public/launchpad/discord.png";
+import meta from "../../public/launchpad/meta.png";
 import styles from "../../styles/launchpad.module.scss";
 import "animate.css";
-import axios from 'axios';
+import axios from "axios";
 
 const Stake = () => {
   const { t } = useTranslation("common");
   const router = useRouter();
 
+  // useEffect(() => {
+  //   updateBalance();
+  //   const timer = setInterval(async () => {
+  //     updateBalance();
+  //   }, 5000);
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
 
-  useEffect(async() => {
-    updateBalance()
-    const timer = setInterval(async () => {
-      updateBalance()
-    }, 5000);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-
-  const updateBalance = async() => {
+  const updateBalance = async () => {
     let accounts = await window.unisat.getAccounts();
-      if(accounts[0]){
-    
-      } 
-  }
-
+    if (accounts[0]) {
+    }
+  };
 
   return (
     <HeaderFooter activeIndex={2}>
@@ -52,45 +56,298 @@ const Stake = () => {
           <div className={styles.info}>
             <div className={styles.text}>
               <h1>The Launchpad Protocol</h1>
-              <h2>for Everyone!</h2>
-              <p>Biso helps everyone to create their own tokens and token sales in few seconds. Tokens created on Biso will be verified and published on explorer websites.</p>
+              <h2>
+                for Everyone! <span className={styles.version}>Version 1</span>
+              </h2>
               <p>
-                 <Link href="/launchpad/list"><button>
-                  Find Project<i></i>
-                </button></Link>
+                Biso helps everyone to create their own tokens and token sales
+                in few seconds. Tokens created on Biso will be verified and
+                published on explorer websites.
+              </p>
+              <div className={styles.operate}>
+                <Link href="/launchpad/list" passHref>
+                  <button>
+                    Find Project<i></i>
+                  </button>
+                </Link>
                 <a href="#">
                   Learn<i></i>
                 </a>
+              </div>
+            </div>
+          </div>
+          <div className={styles.data}>
+            <b></b>
+            <ul>
+              <li>
+                <Image src={icon1} alt="icon" width={57} height={50} />
+                <h1>1</h1>
+                <h2>Projects</h2>
+                <p>in the last 30 days</p>
+              </li>
+              <li>
+                <Image src={icon2} alt="icon" width={60} height={60} />
+                <h1>0</h1>
+                <h2>Total Users</h2>
+                <p>in the last 30 days</p>
+              </li>
+              <li>
+                <Image src={icon3} alt="icon" width={60} height={60} />
+                <h1>0</h1>
+                <h2>Total Liquidity Raised</h2>
+                <p>in the last 30 days</p>
+              </li>
+              <li>
+                <Image src={icon4} alt="icon" width={64} height={64} />
+                <h1>0</h1>
+                <h2>Total Values Locked</h2>
+                <p>in the last 30 days</p>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.list}>
+            <div className={styles.listItem}>
+              <div className={styles.main}>
+                <div className={styles.banner}>
+                  <Image
+                    src={project}
+                    alt="Remote Image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                  <div className={styles.tag}>
+                    <Image src={hot} width={12} height={12} alt="hot" />
+                    <span>So Hot</span>
+                  </div>
+                </div>
+                <div className={styles.avatar}>
+                  <Image src={avatar} alt="avatar" width={80} height={80} />
+                </div>
+                <div className={styles.name}>FairLady Project</div>
+                <div className={styles.contact}>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={twitter}
+                        alt="twitter"
+                        width={20}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={discord}
+                        alt="discord"
+                        width={24.24}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={telegram}
+                        alt="telegram"
+                        width={23.53}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={github}
+                        alt="github"
+                        width={23.53}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={meta}
+                        alt="meta"
+                        width={25.8}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <p>
+                The decentralized Muon network is like a distributed
+                supercomputer with a universal operating system. It will be run
+                by a global community of node operators, all incentivized by the
+                Muon token. This creates a powerful and secure cloud computation
+                service that can run any app/software and connect to all public
+                blockchains. It in no way replaces blockchains, but rather is a
+                perfect technology-
               </p>
             </div>
-            <div className={styles.data}>
-              <b></b>
-              <ul>
-                <li>
-                  <i></i>
-                  <h1>1</h1>
-                  <h2>Projects</h2>
-                  <p>in the last 30 days</p>
-                </li>
-                <li>
-                  <i></i>
-                  <h1>0</h1>
-                  <h2>Total Users</h2>
-                  <p>in the last 30 days</p>
-                </li>
-                <li>
-                  <i></i>
-                  <h1>0</h1>
-                  <h2>Total Liquidity Raised</h2>
-                  <p>in the last 30 days</p>
-                </li>
-                <li>
-                  <i></i>
-                  <h1>0</h1>
-                  <h2>Total Values Locked</h2>
-                  <p>in the last 30 days</p>
-                </li>
-              </ul>
+            <div className={styles.listItem}>
+              <div className={styles.main}>
+                <div className={styles.banner}>
+                  <Image
+                    src={project}
+                    alt="Remote Image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                  <div className={styles.tag}>
+                    <Image src={hot} width={12} height={12} alt="hot" />
+                    <span>So Hot</span>
+                  </div>
+                </div>
+                <div className={styles.avatar}>
+                  <Image src={avatar} alt="avatar" width={80} height={80} />
+                </div>
+                <div className={styles.name}>FairLady Project</div>
+                <div className={styles.contact}>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={twitter}
+                        alt="twitter"
+                        width={20}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={discord}
+                        alt="discord"
+                        width={24.24}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={telegram}
+                        alt="telegram"
+                        width={23.53}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={github}
+                        alt="github"
+                        width={23.53}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={meta}
+                        alt="meta"
+                        width={25.8}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <p>
+                The decentralized Muon network is like a distributed
+                supercomputer with a universal operating system. It will be run
+                by a global community of node operators, all incentivized by the
+                Muon token. This creates a powerful and secure cloud computation
+                service that can run any app/software and connect to all public
+                blockchains. It in no way replaces blockchains, but rather is a
+                perfect technology-
+              </p>
+            </div>
+            <div className={styles.listItem}>
+              <div className={styles.main}>
+                <div className={styles.banner}>
+                  <Image
+                    src={project}
+                    alt="Remote Image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                  <div className={styles.tag}>
+                    <Image src={hot} width={12} height={12} alt="hot" />
+                    <span>So Hot</span>
+                  </div>
+                </div>
+                <div className={styles.avatar}>
+                  <Image src={avatar} alt="avatar" width={80} height={80} />
+                </div>
+                <div className={styles.name}>FairLady Project</div>
+                <div className={styles.contact}>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={twitter}
+                        alt="twitter"
+                        width={20}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={discord}
+                        alt="discord"
+                        width={24.24}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={telegram}
+                        alt="telegram"
+                        width={23.53}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={github}
+                        alt="github"
+                        width={23.53}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                  <Link href="https://www.baidu.com/" passHref>
+                    <a className={styles.item}>
+                      <Image
+                        src={meta}
+                        alt="meta"
+                        width={25.8}
+                        height={20}
+                      ></Image>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <p>
+                The decentralized Muon network is like a distributed
+                supercomputer with a universal operating system. It will be run
+                by a global community of node operators, all incentivized by the
+                Muon token. This creates a powerful and secure cloud computation
+                service that can run any app/software and connect to all public
+                blockchains. It in no way replaces blockchains, but rather is a
+                perfect technology-
+              </p>
             </div>
           </div>
         </div>
--- pages/launchpad/list.js
+++ pages/launchpad/list.js
@@ -10,7 +10,7 @@ import classNames from "classnames/bind";
 import Cookies from "js-cookie";
 import { utils } from "ethers";
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
@@ -18,85 +18,85 @@ import "swiper/css/thumbs";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "../../styles/launchpad_list.module.scss";
 import "animate.css";
-import axios from 'axios';
+import axios from "axios";
 
 const Stake = () => {
   const { t } = useTranslation("common");
   const router = useRouter();
 
+  // useEffect(async () => {
+  //   updateBalance();
+  //   const timer = setInterval(async () => {
+  //     updateBalance();
+  //   }, 5000);
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
 
-  useEffect(async() => {
-    updateBalance()
-    const timer = setInterval(async () => {
-      updateBalance()
-    }, 5000);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-
-  const updateBalance = async() => {
+  const updateBalance = async () => {
     let accounts = await window.unisat.getAccounts();
-      if(accounts[0]){
-    
-      } 
-  }
-
+    if (accounts[0]) {
+    }
+  };
 
   return (
     <HeaderFooter activeIndex={2}>
       <ToastContainer />
       <div className={styles.wrapper}>
         <div className={styles.inner}>
-            <ul className={styles.projects}>
-                <Link href="/launchpad/detail1"><li>
-                    <span>
-                        <h1>
-                            <img src="/launchpad/arks.jpg" />
-                        </h1>
-                        <i> <img src="/launchpad/arks.jpg" /></i>
-                        <h2>Arkstart</h2>
-                        <dt>
-                          <a href="https://twitter.com/arkscoin" target="_blank"  rel="noreferrer"><dl className={styles.twitter}></dl></a>
-                          <a href="https://t.me/ArkstartOfficial" target="_blank"  rel="noreferrer"><dl className={styles.telegarm}></dl></a>
-                          <a href="https://medium.com/@arkstart" target="_blank"  rel="noreferrer"><dl className={styles.medium}></dl></a>
-                        </dt>
-                    </span>
-                    <p>The first project to adopt the VE (3,3) model on the #BRC20 protocol .</p>
-                </li>
-                </Link>
-                {/* <Link href="/launchpad/detail2"><li>
-                    <span>
-                        <h1>
-                            <img src="/launchpad/ghz2.jpg" />
-                        </h1>
-                        <i> <img src="/launchpad/ghz.jpg" /></i>
-                        <h2>古惑仔(GHZI)</h2>
-                        <dt>
-                          <a href="https://twitter.com/HK_Brother_" target="_blank"  rel="noreferrer"><dl className={styles.twitter}></dl></a>
-                          <a href="https://t.me/BRO10000X" target="_blank"  rel="noreferrer"><dl className={styles.telegarm}></dl></a>
-                        </dt>
-                    </span>
-                    <p>歡迎來到古惑幣世界！這是一個基於隱私的加密貨幣，由一群勇敢的古惑仔創造。</p>
-                </li>
-                </Link>
-                <Link href="/launchpad/detail3"><li>
-                    <span>
-                        <h1>
-                            <img src="/launchpad/iksa.jpg" />
-                        </h1>
-                        <i> <img src="/launchpad/iksa.jpg" /></i>
-                        <h2>Isekai Protocol</h2>
-                        <dt>
-                          <a href="https://twitter.com/isekaiprotocol" target="_blank"  rel="noreferrer"><dl className={styles.twitter}></dl></a>
-                          <a href="https://discord.com/invite/tuxbNWqhmA" target="_blank"  rel="noreferrer"><dl className={styles.discord}></dl></a>
-                        </dt>
-                    </span>
-                    <p>Isekai Metaverse is an AI-driven Web3 ACGN (anime, comics, games, and novels) creator ecosystem. With our open-source creator protocol: Isekai Protocol, we will build a bottom-up ACGN metaverse where creators can use NFT culture legos to participate in community creation and eventually form an NFT content network. Isekai Metaverse provides various creation tools and distribution channels.</p>
-                </li>
-                </Link> */}
-            </ul>
+          <ul className={styles.projects}>
+            <Link href="/launchpad/detail" passHref>
+              <li>
+                <span>
+                  <h1>
+                    <Image
+                      src="/launchpad/arks.jpg"
+                      alt="ark"
+                      width={330}
+                      height={215}
+                    />
+                  </h1>
+                  <i>
+                    <Image
+                      src="/launchpad/arks.jpg"
+                      alt="ark"
+                      width={100}
+                      height={100}
+                    />
+                  </i>
+                  <h2>Arkstart</h2>
+                  <dt>
+                    <a
+                      href="https://twitter.com/arkscoin"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <dl className={styles.twitter}></dl>
+                    </a>
+                    <a
+                      href="https://t.me/ArkstartOfficial"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <dl className={styles.telegarm}></dl>
+                    </a>
+                    <a
+                      href="https://medium.com/@arkstart"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <dl className={styles.medium}></dl>
+                    </a>
+                  </dt>
+                </span>
+                <p>
+                  The first project to adopt the VE (3,3) model on the #BRC20
+                  protocol .
+                </p>
+              </li>
+            </Link>
+          </ul>
         </div>
       </div>
     </HeaderFooter>
